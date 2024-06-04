09e17997487f836313448202328607b24e11e849
Jayesh Gaba <jg9140@srmist.edu.in>
2024-06-04 21:26:27 +0530

    ordering added

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
-
+from django.views.generic import ListView
 
 
 # Create your views here.
@@ -9,6 +9,11 @@ def home(request):
     context = {"posts": Post.objects.all()}
     return render(request, "blog/home.html", context)
 
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
 
 def about(request):
 
